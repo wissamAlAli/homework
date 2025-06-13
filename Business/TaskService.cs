675a81800e122504b5d40b9f645049cbd2135bac
wissamAlAli <wissamalalizero@gmail.com>
2025-06-13 12:22:42 -0700

    Update TaskService.cs

--- Business/TaskService.cs
+++ Business/TaskService.cs
@@ -39,7 +39,7 @@ namespace NTierTodoApp.Business
         // TODO: تنفيذ دالة حذف المهمة
         public void DeleteTask(int id)
         {
-            // TODO: استدعاء دالة الحذف في طبقة DataAccess
+            _repo.Delete(id);
         }
     }
 }
